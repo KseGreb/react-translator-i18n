4de1649bc137d01c3b586ae29bc7209491bea4a1
“KseGreb” <ksegreb@gmail.com>
2023-09-20 10:41:23 -0600

    changes in languege btn

--- src/App.js
+++ src/App.js
@@ -7,15 +7,27 @@ function App() {
   const {i18n, t} = useTranslation();
 
   const toggleLang = () => {
-    i18n.changeLanguage(i18n.language === 'en'? 'ru' : 'en')
+    i18n.changeLanguage(i18n.language === 'en'? 'ru' : 'en');
   }
+  const togLang = () => {
+    i18n.changeLanguage(i18n.language === 'ru'? 'es' : 'ru');
+  }
+  const toggleLanguage = () => {
+    i18n.changeLanguage(i18n.language === 'es'? 'en' : 'es');
+  }
+  // const toggleLang = e = i18n.changeLanguage(i18n.language === e.target.value)
 
   return (
     <div className="App">
-      <h1>{t('Greeting', 'Welcome', 'Happy')}</h1>
-      <button onClick={toggleLang}>
+      <h1>{t('Greeting')}</h1>
+      <h2>{t('Welcome')}</h2>
+      <h2>{t('Happy')}</h2>
+      {/* <button onClick={toggleLang}>
         {i18n.language === 'en' ? "RU" : "EN"}
-      </button>
+      </button> */}
+      <button  onClick={toggleLang}>EN</button>
+      <button  onClick={togLang}>RU</button>
+      <button  onClick={toggleLanguage}>ES</button>
     </div>
   );
 }
